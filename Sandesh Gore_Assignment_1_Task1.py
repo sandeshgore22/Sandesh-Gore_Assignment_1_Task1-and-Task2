80993740a2b35ef6f1b84943d4a2adb83d9f9258
sandeshgore22 <sandeshgore22@gmail.com>
2025-08-12 01:08:50 +0530

    Add files via upload

---
+++ Sandesh Gore_Assignment_1_Task1.py
@@ -0,0 +1,8 @@
+#Basic Calculator
+Choice_1=int(input("Enter the first number:"))
+Choice_2=int(input("Enter the second number:"))
+
+print('Addition:',(Choice_1+Choice_2))
+print('Subtraction:',(Choice_1-Choice_2))
+print('Multiplication:',(Choice_1*Choice_2))
+print('Division:',(Choice_1/Choice_2))
---
+++ Sandesh Gore_Assignment_1_Task2.py
@@ -0,0 +1,6 @@
+#Personalized greeting
+fname=input('First Name:')
+lname=input('Last Name:')
+
+#Concatenate
+print(f'Hello {fname} {lname}! Welcome to the Python program.')
